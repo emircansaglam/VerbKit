6a05952513d8b6e3c1437714101e13fafe52553b
Emircan Sağlam <77593482+emircansaglam@users.noreply.github.com>
2026-02-04 11:59:56 +0300

    Initial commit

---
+++ VerbKit/VerbKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerbKitApp.swift
+//  VerbKit
+//
+//  Created by emircan.saglam on 4.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct VerbKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
